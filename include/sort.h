b3a41e6b836af41b53dac419779674f748e5ee87
hoyleeson <hoyleeson@gmail.com>
2018-04-19 15:56:56 +0800

    add sort

---
+++ include/sort.h
@@ -0,0 +1,22 @@
+/*
+ * include/sort.h
+ *
+ * 2016-01-01  written by Hoyleeson <hoyleeson@gmail.com>
+ *	Copyright (C) 2015-2016 by Hoyleeson.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2.
+ *
+ */
+
+#ifndef _ANZZC_SORT_H
+#define _ANZZC_SORT_H
+
+#include <sys/types.h>
+
+void sort(void *base, size_t num, size_t size,
+	  int (*cmp)(const void *, const void *),
+	  void (*swap)(void *, void *, int));
+
+#endif
